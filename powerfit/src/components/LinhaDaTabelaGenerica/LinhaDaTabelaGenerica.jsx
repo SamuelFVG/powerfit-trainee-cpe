b8aa5d32a7acc7209cb13699caf5ba03ebf31cad
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-05-17 21:16:03 -0300

    Linha da tabela Samuel

--- powerfit/src/components/LinhaDaTabelaGenerica/LinhaDaTabelaGenerica.jsx
+++ powerfit/src/components/LinhaDaTabelaGenerica/LinhaDaTabelaGenerica.jsx
@@ -1,9 +1,20 @@
 import {LinhaDaTabelaG} from "./Styles"
 
 
-export default function LinhaDaTabelaGenerica({data}) {
-    return (<>
-        <div>palavras apenas</div>    
-        <div>{data.name}</div>
+export default function LinhaDaTabelaGenerica({dados}) {
+    
+
+    return (<>  
+        <div>{dados.nome}</div>
+        <div>{dados.cargo}</div>
+        <div>{dados.cor_doodle}</div>
+        <div>{dados.atividade}</div>
+        <div>{dados.horas}</div>
     </>)
 }
+
+// nome: 'Nome genérico',
+// cargo: 'Cargo genérico',
+// cor_doodle: '#aaa',
+// atividade: 'Atividade Genérica',
+// horas: 1234
--- powerfit/src/pages/Home/Home.jsx
+++ powerfit/src/pages/Home/Home.jsx
@@ -3,12 +3,12 @@ import {BotaoGenerico,InputGenerico,LinkGenerico,Header,LinhaDaTabelaGenerica } 
 
 
 export default function Home() {
-    const obj =  {
+    const obj =  { //vetor de objetos no final
         nome: 'Nome genérico',
         cargo: 'Cargo genérico',
         cor_doodle: '#aaa',
         atividade: 'Atividade Genérica',
-        horas: 1234
+        horas: '12:34'
     }
     
     return (<div>
@@ -16,7 +16,7 @@ export default function Home() {
         <Titulo>Home</Titulo>
         <BotaoGenerico/>
         <LinkGenerico/> */}
-        <LinhaDaTabelaGenerica data={obj}/>
+        <LinhaDaTabelaGenerica dados={obj}/>
         <InputGenerico/>
     </div>)
            
