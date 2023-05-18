717b3b3b27d6fab9b5059256b54d789b32a6d26f
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-05-18 16:39:41 -0300

    formatação

--- powerfit/src/components/LinhaDaTabelaGenerica/Styles.js
+++ powerfit/src/components/LinhaDaTabelaGenerica/Styles.js
@@ -1,43 +1,42 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const LinhaDaTabelaG = styled.div`
-    /* border: solid 1px green ; */
-    margin-top: 1.5rem;
-    margin-bottom: 1.5rem;
+  /* border: solid 1px green ; */
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
 
-    height: 50px;
-    width: 100%; 
-    display: flex;  
-    gap: 25%;
-    justify-content: center;
-`;  
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
 
 export const Perfil = styled.div`
-    width: 200px;
-    gap: 3%;
+  width: 200px;
+  gap: 3%;
 
-    display: flex;
-    justify-content: left;
+  display: flex;
+  justify-content: left;
 `;
 
 export const InformacoesPerfil = styled.div`
-    flex-grow: 1;    
+  flex-grow: 1;
 
-    display: flex;
-    flex-direction: column; 
+  display: flex;
+  flex-direction: column;
 `;
 
 export const DivGenerico = styled.div`
-    flex-grow: inherit;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  flex-grow: inherit;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-    
-export const Doodle = styled.div` 
-    background-color: #6E6D9C;
-    border-radius: 100%;
-    height: 100%;
-    aspect-ratio: 1 / 1;
+
+export const Doodle = styled.div`
+  background-color: #6e6d9c;
+  border-radius: 100%;
+  height: 100%;
+  aspect-ratio: 1 / 1;
 `;
--- powerfit/src/pages/Home/Home.jsx
+++ powerfit/src/pages/Home/Home.jsx
@@ -1,41 +1,49 @@
-import {Titulo,} from "./Styles"
-import { Tabela } from "./Styles"
-import { TopoTabela } from "./Styles"
-import { DivGenerico } from "../../components/LinhaDaTabelaGenerica/Styles"
+import { Titulo } from "./Styles";
+import { Tabela } from "./Styles";
+import { TopoTabela } from "./Styles";
+import { DivGenerico } from "../../components/LinhaDaTabelaGenerica/Styles";
 
-import {BotaoGenerico,InputGenerico,LinkGenerico,Header,LinhaDaTabelaGenerica }  from "../../components"
+import {
+  BotaoGenerico,
+  InputGenerico,
+  LinkGenerico,
+  Header,
+  LinhaDaTabelaGenerica,
+} from "../../components";
 
-
-const obj =  { //vetor de objetos no final
-    nome: 'Nome genérico',
-    cargo: 'Cargo genérico',
-    cor_doodle: '#aaa',
-    atividade: 'Atividade Genérica',
-    horas: '12:34'
-}
+const obj = {
+  //vetor de objetos no final
+  nome: "Nome genérico",
+  cargo: "Cargo genérico",
+  cor_doodle: "#aaa",
+  atividade: "Atividade Genérica",
+  horas: "12:34",
+};
 
 export default function Home() {
-    
-    return (<div>
-    <Tabela>
+  return (
+    <div>
+      <Tabela>
         <TopoTabela>
-            <DivGenerico>Perfil</DivGenerico>
-            <DivGenerico>Atividade</DivGenerico>
-            <DivGenerico>Tempo</DivGenerico>
+          <DivGenerico>Perfil</DivGenerico>
+          <DivGenerico>Atividade</DivGenerico>
+          <DivGenerico>Tempo</DivGenerico>
         </TopoTabela>
-        <LinhaDaTabelaGenerica dados={obj}/>
-        <LinhaDaTabelaGenerica dados={obj}/>
-        <LinhaDaTabelaGenerica dados={obj}/>
-        <LinhaDaTabelaGenerica dados={obj}/>
-        <LinhaDaTabelaGenerica dados={obj}/>
-        <LinhaDaTabelaGenerica dados={obj}/>
-    </Tabela>
-        
-    </div>)
+        <LinhaDaTabelaGenerica dados={obj} />
+        <LinhaDaTabelaGenerica dados={obj} />
+        <LinhaDaTabelaGenerica dados={obj} />
+        <LinhaDaTabelaGenerica dados={obj} />
+        <LinhaDaTabelaGenerica dados={obj} />
+        <LinhaDaTabelaGenerica dados={obj} />
+      </Tabela>
+    </div>
+  );
 }
 
-{/* <Header/>
+{
+  /* <Header/>
 <Titulo>Home</Titulo>
 <BotaoGenerico/>
 <LinkGenerico/> 
-<InputGenerico/> */}
+<InputGenerico/> */
+}
