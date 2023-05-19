738de51be3e3d60df103147276b6b77225ca20b6
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-05-19 17:08:37 -0300

    Mudança no tamanho da fonte no globalstyles

--- powerfit/src/GlobalStyles.js
+++ powerfit/src/GlobalStyles.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyles = createGlobalStyle`
   html, body {
     font-family: "Noto Sans", sans-serif;
-    font-size: 14px;
+    font-size: 15px;
     background-color: #0A0A16;
     color: #F2F1FB;
 
