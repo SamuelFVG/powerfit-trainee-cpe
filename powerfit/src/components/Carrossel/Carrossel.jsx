26717a9f82bf354eaf34fdb2bbda286a5a1ffa48
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-05-26 19:04:15 -0300

    Refatoração carrossel e seus estilos

--- powerfit/src/components/Carrossel/Carrossel.jsx
+++ powerfit/src/components/Carrossel/Carrossel.jsx
@@ -1,29 +1,16 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper";
 
-import Slide_1 from "../../assets/Slide_1.svg";
-import Slide_2 from "../../assets/Slide_2.svg";
-import Slide_3 from "../../assets/Slide_3.svg";
-
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import { ImagemCarrossel, SwiperContainer } from "./Style";
 
+import Slide_1 from "../../assets/Slide_1.svg";
+import Slide_2 from "../../assets/Slide_2.svg";
+import Slide_3 from "../../assets/Slide_3.svg";
 const slides = [Slide_1, Slide_2, Slide_3];
-
-function montaSlides() {
-  let saidaSlides = [];
-  for (let i = 0; i < slides.length; i++) {
-    saidaSlides.push(
-      <SwiperSlide key={i}>
-        <ImagemCarrossel src={slides[i]} />
-      </SwiperSlide>
-    );
-  }
-  return saidaSlides;
-}
 
 export default function Carrossel() {
   return (
@@ -32,8 +19,10 @@ export default function Carrossel() {
         modules={[Autoplay, Navigation, Pagination]}
         navigation
         pagination={{ clickable: true }}
+        //faz rodar infinito
         loop
-        autoplay={{ delay: 5000, disableOnInteraction: false }}
+        autoplay={{ delay: 5000 }}
+        //decidi criar os estilos aqui mexendo nas custom properties ao invés de criar um css pro carrossel
         style={{
           "--swiper-navigation-color": "#F2F1FB",
           "--swiper-navigation-size": "2rem",
@@ -47,7 +36,12 @@ export default function Carrossel() {
           "--swiper-pagination-bullet-height": "0.7rem",
         }}
       >
-        {montaSlides()}
+        {/* Defina cada imagem como swiperslide e coloque seu index como chave (sem a chave, dá erro) */}
+        {slides.map((slide) => (
+          <SwiperSlide key={slides.indexOf(slide)}>
+            <ImagemCarrossel src={slide} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </SwiperContainer>
   );
--- powerfit/src/components/Carrossel/Style.js
+++ powerfit/src/components/Carrossel/Style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ImagemCarrossel = styled.img`
-  width: 100%;
+  width: 100%; //a imagem será tão grande quanto o carrossel. Sua altura muda automaticamente
   border-radius: 10px;
 `;
 
