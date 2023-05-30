1698438350114f819e7fc5b2ca972c736783a86c
Samuel Felipe Verçosa Gonçalves <samuelfelipeif@gmail.com>
2023-05-30 07:48:54 -0300

    Início da linkagem

--- powerfit/src/pages/Login/Login.jsx
+++ powerfit/src/pages/Login/Login.jsx
@@ -9,25 +9,26 @@ import {
   Texto,
 } from "./Styles";
 import { LogoGenerica, BotaoGenerico, Header } from "../../components";
+import { BotaoG } from "../../components/BotaoGenerico/Styles";
+import { useState } from "react";
+import api from "../../services/api";
 
 export default function CadastroPage() {
-  const [form, setForm] = React.useState({ email: "", senha: "" });
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [carregando, setCarregando] = useState(false);
 
-  function atualizaForm(event) {
-    setForm({ ...form, [event.target.name]: event.target.value });
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ email, senha });
 
-  const body = {
-    email: form.email,
-    senha: form.senha,
+    try {
+      const res = await api.post("/login", { email, senha });
+      console.log(res);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
-
-  console.log(body);
-
-  function goTo(page) {
-    navigate(page);
-  }
 
   return (
     <>
@@ -41,30 +42,33 @@ export default function CadastroPage() {
               rota="?"
             />
           }
-          <form>
+          <form onSubmit={handleSubmit}>
             <Texto>Email:</Texto>
             <InputForm
               placeholder="exemplo@*****.com"
               type="email"
               name="email"
-              value={form.email}
-              onChange={(event) => atualizaForm(event)}
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
               required
             ></InputForm>
+
             <Texto>Senha:</Texto>
             <InputForm
               placeholder="********"
               type="password"
               name="senha"
-              value={form.senha}
-              onChange={(event) => atualizaForm(event)}
+              id="senha"
+              onChange={(e) => setSenha(e.target.value)}
               required
             ></InputForm>
 
             <DivSubmitButton>
-              <BotaoGenerico texto="Login" rota={"---"} />
+              {/* <BotaoGenerico texto="Login" type="submit" /> */}
+              <BotaoG type="submit">Login</BotaoG>
             </DivSubmitButton>
           </form>
+
           <Link to={"/cadastro"}>
             <PalavraLink>Não possui conta? Cadastre-se!</PalavraLink>
           </Link>
@@ -73,3 +77,45 @@ export default function CadastroPage() {
     </>
   );
 }
+
+{
+  /* <form>
+  <Texto>Email:</Texto>
+  <InputForm
+    placeholder="exemplo@*****.com"
+    type="email"
+    name="email"
+    value={form.email}
+    onChange={(event) => atualizaForm(event)}
+    required
+  ></InputForm>
+  <Texto>Senha:</Texto>
+  <InputForm
+    placeholder="********"
+    type="password"
+    name="senha"
+    value={form.senha}
+    onChange={(event) => atualizaForm(event)}
+    required
+  ></InputForm>
+
+  <DivSubmitButton>
+    <BotaoGenerico texto="Login" type="submit" />
+  </DivSubmitButton>
+</form>; */
+}
+
+// const [form, setForm] = React.useState({ email: "", senha: "" });
+// const navigate = useNavigate();
+// function atualizaForm(event) {
+//   setForm({ ...form, [event.target.name]: event.target.value });
+// }
+
+// const body = {
+//   email: form.email,
+//   senha: form.senha,
+// };
+// console.log(body);
+// function goTo(page) {
+//   navigate(page);
+// }
